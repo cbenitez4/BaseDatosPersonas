c1f00e08f8e7028c372f613adc9998b7fdbe0fa6
Carlos <carlosbenitez1995@gmail.com>
2017-05-13 11:53:37 -0500

    El mejor comit n2

---
+++ app/src/main/java/com/example/labsoftware1/personasbasededatos/Datos.java
@@ -0,0 +1,42 @@
+package com.example.labsoftware1.personasbasededatos;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Carlos on 13/05/2017.
+ */
+
+public class Datos {
+    public static ArrayList<Persona> traerPersonas(Context contexto){
+        ArrayList<Persona> personas = new ArrayList<>();
+        SQLiteDatabase db;
+        String sql,foto, nombre, cedula, apellido, sexo, pasatiempo;
+        Persona p;
+
+        PersonaSQLiteOpenHelper aux = new PersonaSQLiteOpenHelper(contexto,"DBPersonas",null,2);
+        db =aux.getReadableDatabase();
+
+        sql="select * from Personas";
+        Cursor c = db.rawQuery(sql,null);
+
+        if (c.moveToFirst()){
+            do {
+                foto = c.getString(0);
+                cedula = c.getString(1);
+                nombre = c.getString(2);
+                apellido = c.getString(3);
+                sexo = c.getString(4);
+                pasatiempo = c.getString(5);
+                p = new Persona(foto,cedula,nombre,apellido,sexo,pasatiempo);
+                personas.add(p);
+
+            }while (c.moveToNext());
+        }db.close();
+        return personas;
+    }
+
+}
